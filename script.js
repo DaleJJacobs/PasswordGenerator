4279dcdeea79a40b80f2585acfb8ca6de294188e
Dale J <dalejohn33@gmail.com>
2021-01-10 11:36:35 -0500

    Final push, cleaning up pseudo code and commenting out console logs

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ generateBtn.addEventListener("click", generatePassword);
 function generatePassword() {
   var confirmLength = prompt("How many characters would you like your password to contain?  Must be between 8-128 characters.");
 
-  // Loop if answer is outside the parameters 
+  // Loop if answer is not between 8 and 128 
   while (parseInt(confirmLength) <= 7 || parseInt(confirmLength) >= 129) {
     alert("Password length must be between 8-128 characters.  Please try again");
     var confirmLength = prompt("How many characters would you like in your password?  8-128 please.");
   }
 
-  // First set of prompts to confirm which characters the user would like
+  // First set of prompts to confirm which characters the user would like to use
   var lowerCase = confirm('Would You like to use lower case characters?');
   console.log('this is lowerCase', lowerCase);
   var upperCase = confirm("Would you like to use upper case characters?")
@@ -27,7 +27,7 @@ function generatePassword() {
   var spChar = confirm("Would you like to use Special Characters?");
   console.log('this is special characters', spChar);
   var numChar = confirm("Would you like to use numbers?");
-  console.log('this is numbers', numChar);
+  // console.log('this is numbers', numChar);
 
   // While loop if user chooses all options as false and 2nd set of prompts
   while (upperCase === false && lowerCase === false && spChar === false && numChar === false) {
@@ -39,7 +39,7 @@ function generatePassword() {
     var spChar = confirm("Would you like to use Special Characters?");
     console.log('this is special characters', spChar);
     var numChar = confirm("Would you like to use numbers?");
-    console.log('this is numbers', numChar);
+    // console.log('this is numbers', numChar);
   }
 
   //  Creation of empty array to plug in choices for characters of new password based on user input
@@ -52,7 +52,7 @@ function generatePassword() {
     var index = Math.floor(Math.random() * alphaUpper.length);
     counter += 1;
     passWORD.push(alphaUpper[index]);
-    console.log('Uppercase', possibleChoices);
+    // console.log('Uppercase', possibleChoices);
   }
   if (lowerCase) {
     //add lowerCase array to possibleChoices array, and a random lower case character to new pasword
@@ -60,7 +60,7 @@ function generatePassword() {
     var index = Math.floor(Math.random() * alphaLower.length);
     counter += 1;
     passWORD.push(alphaLower[index]);
-    console.log("Lowercase", possibleChoices);
+    // console.log("Lowercase", possibleChoices);
   }
   if (spChar) {
     //add spChar array to possibleChoices array, and a random special character to new pasword
@@ -68,7 +68,7 @@ function generatePassword() {
     var index = Math.floor(Math.random() * specialChar.length);
     counter += 1;
     passWORD.push(specialChar[index]);
-    console.log('special characters', possibleChoices);
+    // console.log('special characters', possibleChoices);
   }
   if (numChar) {
     //add numChar array to possibleChoices array, and a random number character to new pasword
@@ -76,17 +76,20 @@ function generatePassword() {
     var index = Math.floor(Math.random() * number.length);
     counter += 1;
     passWORD.push(number[index]);
-    console.log('numbers', possibleChoices);
+    // console.log('numbers', possibleChoices);
   }
-  // Creation of the new password, randomly picked characters from the new array at the lenght designated by the user, then writing that new password to the password ID in the HTML
-  
-  for (var i = 1; i <= confirmLength; i++) {
+  // Creation of the new password, randomly picked characters from the new array at the length designated by the user, then writing that new password to the password ID in the HTML
+
+  for (var i = counter + 1; i <= confirmLength; i++) {
     var index = Math.floor(Math.random() * possibleChoices.length);
-    passWORD += possibleChoices[index];
+    passWORD.push(possibleChoices[index]);
+    // console.log('new password for loop:', passWORD);
   }
 
   console.log('new password', passWORD);
-  document.getElementById("password").innerHTML = passWORD;
+  document.getElementById("password").innerHTML = passWORD.join("");
+
+
 
 }
 
